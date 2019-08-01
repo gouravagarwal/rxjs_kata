ba6e97477716e6525a0fb46ba78e11ef7a3c637a
aggourav <gourav.agarwal@qualfon.com>
2019-08-01 20:13:05 +0530

    Solved failing tesst cases in feature report service

--- src/app/services/feature-report-service/feature-report.service.ts
+++ src/app/services/feature-report-service/feature-report.service.ts
@@ -4,17 +4,28 @@ import { Observable, Subject, BehaviorSubject, ReplaySubject } from 'rxjs';
 import { FeatureReport } from 'src/app/model/feature-report';
 import { FeatureRequestService } from '../feature-request-service/feature-request.service';
 import { map, scan } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeatureReportService {
-  
-  
-  constructor(private _featureRequestService:FeatureRequestService) { }
 
 
-  liveReport(): Observable<FeatureReport> {    
-    return undefined;
+  constructor(private _featureRequestService: FeatureRequestService) { }
+
+
+  liveReport(): Observable<FeatureReport> {
+    let a = new FeatureReport(null, null, null);
+    return this._featureRequestService.getSubscribableNewRequests().pipe(
+      map((x: FeatureRequest) => { a.accumulateItem(x); return a; }),
+      scan((x: FeatureReport, y: FeatureReport) => {
+        let t = new FeatureReport(null, null, null);
+        t.NumberIssues = x.NumberIssues + y.NumberIssues;
+        t.NumberUrgent = x.NumberUrgent + y.NumberUrgent;
+        t.NumberHighComplexity = x.NumberHighComplexity + x.NumberHighComplexity;
+        return t;
+      })
+    );
   }
 }
